c05d187cb6654485781372de2ebb004e83023c04
Vtr.bwr <vrodriguespereira11@gmail.com>
2025-11-24 21:17:37 -0300

    Create script.js

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+//========= menu amburguer ===========//
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+
+  // Fechar menu ao clicar em um link
+  document.querySelectorAll(".nav-link").forEach(link =>
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    })
+  );
+//======= fim CD amburguer  ==========//
